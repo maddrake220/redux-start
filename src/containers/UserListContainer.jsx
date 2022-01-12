ad1af1556b4a6146511c3457fa3f20843bb753da
maddrake220 <jwchoi4118@gmail.com>
2022-01-12 18:08:21 +0900

    redux-actions

--- src/containers/UserListContainer.jsx
+++ src/containers/UserListContainer.jsx
@@ -8,16 +8,6 @@ export default function UserListContainer() {
   const users = useSelector((state) => state.users.data);
   const dispatch = useDispatch();
 
-  //   const getUsers = useCallback(async () => {
-  //     try {
-  //       dispatch(getUsersStart());
-  //       const res = await axios.get("https://api.github.com/users");
-  //       dispatch(getUsersSuccess(res.data));
-  //     } catch (error) {
-  //       dispatch(getUsersFail(error));
-  //     }
-  //   }, [dispatch]);
-
   const getUsers = useCallback(() => {
     dispatch(getUsersSagaStart());
   }, [dispatch]);
--- src/redux/modules/filter.js
+++ src/redux/modules/filter.js
@@ -1,27 +1,50 @@
-// docks pattern
-// action type definition
-const SHOW_ALL = "redux-start/filter/SHOW_ALL";
-const SHOW_COMPLETE = "redux-start/filter/SHOW_COMPLETE";
+import { createActions, handleActions } from "redux-actions";
+// action type definition and action create function
+export const { showAll, showComplete } = createActions(
+  "SHOW_ALL",
+  "SHOW_COMPLETE",
+  {
+    prefix: "redux-start/filter",
+  }
+);
 
-// action create function
-export function showAll() {
-  return { type: SHOW_ALL };
-}
-
-export function showComplete() {
-  return { type: SHOW_COMPLETE };
-}
-
-// initial value
+// initialState
 const initialState = "ALL";
 
 // reducer
-export default function reducer(previousState = initialState, action) {
-  if (action.type === SHOW_COMPLETE) {
-    return "COMPLETE";
-  }
-  if (action.type === SHOW_ALL) {
-    return "ALL";
-  }
-  return previousState;
-}
+const reducer = handleActions(
+  {
+    SHOW_ALL: (state, action) => "ALL",
+    SHOW_COMPLETE: () => "COMPLETE",
+  },
+  initialState,
+  { prefix: "redux-start/filter" }
+);
+
+export default reducer;
+// docks pattern
+// action type definition
+// const SHOW_ALL = "redux-start/filter/SHOW_ALL";
+// const SHOW_COMPLETE = "redux-start/filter/SHOW_COMPLETE";
+
+// action create function
+// export function showAll() {
+//   return { type: SHOW_ALL };
+// }
+
+// export function showComplete() {
+//   return { type: SHOW_COMPLETE };
+// }
+
+// initial value
+
+// // reducer
+// export default function reducer(previousState = initialState, action) {
+//   if (action.type === SHOW_COMPLETE) {
+//     return "COMPLETE";
+//   }
+//   if (action.type === SHOW_ALL) {
+//     return "ALL";
+//   }
+//   return previousState;
+// }
